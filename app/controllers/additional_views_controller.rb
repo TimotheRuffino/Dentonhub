f4d2c901dd2e666e392fdd72545a68eec7efa26d
TimotheRuffino <timothe.ruffino@gmail.com>
2021-06-27 21:08:48 +0200

    add contact page

---
+++ app/controllers/additional_views_controller.rb
@@ -0,0 +1,2 @@
+class AdditionalViewsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :landing_page
   root to: "landing_page#index"
+  get '/contact', to: 'additional_views#contact', as: 'contact'
 end
